58a1d0b03330eb65b933ad4d5c95e9d1f6dae860
moonjiyoon <ske0830@gmail.com>
2021-08-28 02:13:45 +0900

    app

--- library/app.py
+++ library/app.py
@@ -2,25 +2,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-db = SQLAlchemy()
 
-def create_app():
-    app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://kdt:kdt_password@localhost:3306/library?charset=utf8mb4"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config['SESSION_TYPE'] = 'filesystem'
-    app.secret_key = "123"
 
-    db.init_app(app)
-    migrate = Migrate(app, db)
+app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://kdt:kdt_password@localhost:3306/library?charset=utf8mb4"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+app.secret_key = "123"
 
-    from view import user_api, api
+db.init_app(app)
+migrate = Migrate(app, db)
 
-    app.register_blueprint(api.bp)
-    app.register_blueprint(user_api.bp)
+from view import user_api, api
 
-    return app
+app.register_blueprint(api.bp)
+app.register_blueprint(user_api.bp)
+
 
 if __name__ == "__main__":
-    create_app().run(debug=True)
+    app.run(debug=True)
